226c952993ee3bdd7ef0e26b0f1bf056c4dbd32a
MD Abidur Rahman <89156929+abid9837@users.noreply.github.com>
2024-02-14 16:18:31 +0600

    Delete asset directory

--- asset/js/language.js
+++
@@ -1,7 +0,0 @@
-
-
-function languaage_select(){ 
-    var Language = jQuery('#Select_language').val();
-    window.location.replace("http://localhost/passport%20validation%20system/asset/ajax.php/language.make.session.php/?language="+Language);
-    
-}
